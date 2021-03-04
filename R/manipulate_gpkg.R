c318915ddb060c254c96cd6293e92fa489ace0e3
florisvdh <floris.vanderhaeghe@inbo.be>
2021-03-04 16:29:45 +0100

    set_timestamp_gpkg(): add examples for vector & raster

--- R/manipulate_gpkg.R
+++ R/manipulate_gpkg.R
@@ -19,6 +19,74 @@
 #' @return
 #' Value \code{0} is returned invisibly.
 #'
+#' @examples
+#' library(openssl)
+#' md5sum <- function(x) paste(md5(file(x)))
+#'
+#' # Using existing geopackage with vector layer:
+#' filepath <- system.file("gpkg/b_pump.gpkg", package = "sf")
+#' (md5_original <- md5sum(filepath))
+#'
+#' sf_layer <- read_sf(system.file("gpkg/b_pump.gpkg", package = "sf"))
+#'
+#' # A rewrite changes the checksum:
+#' filepath_notimeset <- file.path(tempdir(), "b_pump_notimeset.gpkg")
+#'   # write 1:
+#' st_write(sf_layer, dsn = filepath_notimeset)
+#' (md5_notimeset1 <- md5sum(filepath_notimeset))
+#'   # write 2:
+#' st_write(sf_layer, dsn = filepath_notimeset, delete_dsn = TRUE)
+#' (md5_notimeset2 <- md5sum(filepath_notimeset))
+#'   # compare:
+#' md5_notimeset1 == md5_notimeset2
+#'
+#' # Setting a fixed date/time
+#' (fixed_date <- as.POSIXct("2020-12-25"))
+#' (fixed_time <- as.POSIXct("2020-12-25 12:00:00", tz = "CET"))
+#'
+#' filepath_timeset <- file.path(tempdir(), "b_pump_timeset.gpkg")
+#'   # write 1 (date):
+#' st_write(sf_layer, dsn = filepath_timeset)
+#' set_timestamp_gpkg(filepath_timeset, fixed_date)
+#' md5_timeset1 <- md5sum(filepath_timeset)
+#'   # write 2 (date):
+#' st_write(sf_layer, dsn = filepath_timeset, delete_dsn = TRUE)
+#' set_timestamp_gpkg(filepath_timeset, fixed_date)
+#' md5_timeset2 <- md5sum(filepath_timeset)
+#'   # write 3 (time):
+#' st_write(sf_layer, dsn = filepath_timeset, delete_dsn = TRUE)
+#' set_timestamp_gpkg(filepath_timeset, fixed_time)
+#' md5_timeset3 <- md5sum(filepath_timeset)
+#'   # write 4 (time):
+#' st_write(sf_layer, dsn = filepath_timeset, delete_dsn = TRUE)
+#' set_timestamp_gpkg(filepath_timeset, fixed_time)
+#' md5_timeset4 <- md5sum(filepath_timeset)
+#'   # compare:
+#' all.equal(md5_timeset1, md5_timeset2, md5_timeset3, md5_timeset4)
+#'
+#' # Also works for GPKG with GDAL raster driver (accessed with stars):
+#' library(stars)
+#' library(dplyr)
+#'
+#' filepath_stars <- file.path(tempdir(), "stars_2d.gpkg")
+#'
+#' stars_2d <-
+#' 	system.file("tif/L7_ETMs.tif", package = "stars") %>%
+#' 	read_stars() %>%
+#' 	slice(band, 1)
+#'   # write 1:
+#' stars_2d %>%
+#' 	write_stars(filepath_stars, driver = "GPKG")
+#' set_timestamp_gpkg(filepath_stars, fixed_time)
+#' md5_stars1 <- md5sum(filepath_stars)
+#'   # write 2:
+#' stars_2d %>%
+#' 	write_stars(filepath_stars, driver = "GPKG")
+#' set_timestamp_gpkg(filepath_stars, fixed_time)
+#' md5_stars2 <- md5sum(filepath_stars)
+#'   # compare:
+#' all.equal(md5_stars1, md5_stars2)
+#'
 #' @author Floris Vanderhaeghe, \url{https://github.com/florisvdh}
 #'
 #' @export
